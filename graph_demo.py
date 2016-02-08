12f882ef1da6f7ef44a4bbf2c4dab5433c2d0172
Vesa Kuoppala <vesa@parkkisahko.fi>
2016-02-08 11:37:16 +0200

    graph class

---
+++ graph_demo.py
@@ -0,0 +1,37 @@
+'''
+Created on 6 Feb 2016
+
+@author: vesakuoppala
+'''
+
+graph = { "a" : ["c"],
+          "b" : ["c", "e"],
+          "c" : ["a", "b", "d", "e"],
+          "d" : ["c"],
+          "e" : ["c", "b"],
+          "f" : [],
+         }
+
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+    return edges
+
+print(generate_edges(graph))
+
+def find_isolated_nodes(graph):
+    """
+    returns a list of isolated nodes. 
+    """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
+
+
+print(find_isolated_nodes(graph))
+
+
